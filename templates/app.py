3b7bf3fff8786a7ae189228db11d181d07471eb1
mmastin <matthew.mastin@gmail.com>
2019-07-22 18:16:19 -0400

    minor additions

--- templates/app.py
+++ templates/app.py
@@ -4,6 +4,8 @@ from flask import Flask
 def create_app():
 	'''Create and configure an instance of the Flask application'''
 	app = Flask(__name__)
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+	DB.init_app(app)
 	
 	@app.route('/')
 	def root():
